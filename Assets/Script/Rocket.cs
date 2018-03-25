956a63475f07b511f1831f82f4049d1e4ef54712
LAPTOP-DEVON-20\dtoland <devon.toland@live.com>
2018-03-25 13:39:08 -0500

    Fixed Lighting added time delay and State enaum

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
@@ -9,6 +10,9 @@ public class Rocket : MonoBehaviour {
     [SerializeField] float mainTrust = 100f;
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
 
 	// Use this for initialization
 	void Start () {
@@ -18,23 +22,40 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Thrust();
-        Rotate();
+        // todo somewhere stop sound on death
+        if( state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
 	}
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; } //if dead return
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK"); //todo remove
                 break;
-            case "Fuel":
-                print("fuel");
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f); // parameterise time
                 break;
             default:
-                print("dead"); //todo kill player
+                state = State.Dying;
+                print("hit something deadly");
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0); // todo allow for more than 2 levels
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void Thrust()
@@ -57,7 +78,6 @@ public class Rocket : MonoBehaviour {
     private void Rotate()
     {
         rigidBody.freezeRotation = true; //take manual control of rotation
-
         float rotationThisFrame = rcsthrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
